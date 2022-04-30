767d2b88f7b15dcbc7464772668e2f5baa0849a1
yagobbosa <yagobbosa@yahoo.com>
2022-04-29 21:11:15 -0300

    created: projeto

---
+++ mudi/src/test/java/com/yagobbosa/mudi/MudiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yagobbosa.mudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
